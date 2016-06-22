8993a1d8bf404a02f45f079411fa1ea61442c38b
Fadime Özhan <fadime_ozhan@hotmail.com>
2016-06-22 17:36:53 +0300

    Initial Commit

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,50 @@
+class RecipesController < ApplicationController
+  before_action :find_recipe, only: [:show,:edit,:update,:destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @recipes = Recipe.all.order('created_at Desc')
+  end
+
+  def new
+    @recipe = current_user.recipes.build
+  end
+
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+
+    if @recipe.save
+      redirect_to @recipe, notice: "Successfully created new recipe."
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe, notice: "Successfully updated recipe."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    redirect_to root_path, notice: "Successfully deleted recipe"
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, :description, :image, ingredients: [:id,:name,:_destroy], directions: [:id,:step,:_destroy])
+  end
+
+  def find_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+end
